db67c015d0b6992f826fc356e340c9fa8825027d
Felix Chan <felixchan29@gmail.com>
2019-05-20 17:59:36 -0400

    portfolio page components and containers

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Portfolio from './containers/Portfolio';
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const darkgrayStyle = {
+  background:"#262626",
+  color:"white",
+};
+
+class App extends Component {
+
+  render() {
+    return (
+      <Router>
+        <React.Fragment>
+          {/* <div style={darkgrayStyle}>
+            <NavBar />
+          </div> */}
+
+          <Switch>
+            <Route path="/" render={(props) => <Portfolio />} />
+          </Switch>
+
+          {/* <Footer /> */}
+        </React.Fragment>
+      </Router>
+    );
+  }
 }
+
 
 export default App;
---
+++ src/containers/Portfolio.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import './Portfolio.scss';
+import { Container, Responsive } from 'semantic-ui-react'
+import Me from '../components/Me'
+import Project from '../components/Project_Squares'
+import typing from '../assets/typing.gif'
+
+class MainPage extends Component {
+  constructor(props){
+    super(props)
+    this.state ={
+    }
+  }
+
+  render(){
+    return (
+    <div >
+      <div className="container">
+        <img src={typing} className="image" alt=""/>
+        <div className="overlay">
+          <div className="text">
+            <h1>Hello, I'm Felix Chan</h1>
+            <h2>FullStack Web Developer</h2>
+          </div>
+        </div>
+      </div>
+
+      <div className="grayStyle">
+        <Container textAlign='center'>
+          {/* <About /> */}
+        </Container>
+      </div>
+
+      <Container textAlign='center'>
+        {/* <Education /> */}
+      </Container>
+
+      <div className="grayStyle">
+        {/* <Skills /> */}
+      </div>
+
+      <Container textAlign='center'>
+        {/* <Intrests /> */}
+      </Container>
+
+      <div className="grayStyle">
+        <Container textAlign='center'>
+          {/* <Contact /> */}
+        </Container>
+      </div>
+
+    </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+
+  }
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(MainPage)
