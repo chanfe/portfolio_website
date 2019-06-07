95e1f20f7740cd7c449e4acf425428bf563a5a59
Felix Chan <felixchan29@gmail.com>
2019-06-07 13:22:17 -0400

    add meta to phone only to have a better fit on iphones

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.scss';
 import Portfolio from './containers/Portfolio';
+import { Responsive, Button } from 'semantic-ui-react'
 
 import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
@@ -10,7 +11,10 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <React.Fragment>          
+        <React.Fragment>
+            <Responsive {...Responsive.onlyMobile}>
+              <meta name="viewport" content="initial-scale=1, maximum-scale=1" />
+            </Responsive>          
           <Switch>
             <Route path="/" render={(props) => <Portfolio />} />
           </Switch>
