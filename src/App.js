58115c63660af50082066803c55fd50adda96d1d
Felix Chan <felixchan29@gmail.com>
2019-06-09 22:38:52 -0400

    add og to have linkedin image show up

--- src/App.js
+++ src/App.js
@@ -11,6 +11,10 @@ class App extends Component {
   render() {
     return (
       <Router>
+        <meta property='og:title' content="Felix Chan portfolio"/>
+        <meta property='og:image' content="./assets/typing.gif"/>
+        <meta property='og:description' content="This is my portfolio"/>
+        <meta property='og:url' content="https://felixchan.netlify.com/" />
         <React.Fragment>
             <Responsive {...Responsive.onlyMobile}>
               <meta name="viewport" content="initial-scale=1, maximum-scale=1" />
