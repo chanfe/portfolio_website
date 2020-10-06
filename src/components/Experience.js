b5cc9f3040ad23fc5898e31100c7e19caa7f8689
Felix Chan <felixchan29@gmail.com>
2020-10-06 09:03:15 -0400

    update experiance

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -19,6 +19,30 @@ class Experience extends Component {
               </li>
             </ul>
           </p>
+          <h3>U.S. Census Bureau, New York, NY, Aug 2020 – Oct 2020</h3>
+            <h4> Enumerator / SBE Enumerator	</h4>
+            <p>
+              <ul>
+                <li>
+                Planned work by reviewing assignment areas to determine organization of neighborhoods and location households for conducting interviews.
+                </li>
+                <li>
+                Conducted interviews with residents in assigned areas by following stringent guidelines and confidentiality laws.  Explained the purpose of the census interview, answered questions, elicited information following a script, and recorded census data on forms.
+                </li>
+                <li>
+                Assessed quality control levels on selected addresses, determined which samples passed or failed, and maintained records for quality control verification.
+                </li>
+                <li>
+                Complied with accuracy standards while maintaining high production rates.
+                </li>
+                <li>
+                Maintained records of hours worked, unit produced, miles driven, quality control results, and expenses incurred in the performance of duties.
+                </li>
+                <li>
+                Met daily with the supervisor to review and submit work, and receive additional instructions.
+                </li>
+              </ul>
+            </p>
           <h3>Full Stack Progressive, New York, NY, Aug 2019 – Mar 2020</h3>
             <h4>Full Stack Web Developer Participate</h4>
             <p>
@@ -80,6 +104,30 @@ class Experience extends Component {
                   <ul>
                     <li>
                     A remote Full Stack Developer and working on modernizing PlateRate frontend by implementing a better website design.              
+                    </li>
+                  </ul>
+                </p>
+                <h3>U.S. Census Bureau, New York, NY, Aug 2020 – Oct 2020</h3>
+                <h4> Enumerator / SBE Enumerator	</h4>
+                <p>
+                  <ul>
+                    <li>
+                    Planned work by reviewing assignment areas to determine organization of neighborhoods and location households for conducting interviews.
+                    </li>
+                    <li>
+                    Conducted interviews with residents in assigned areas by following stringent guidelines and confidentiality laws.  Explained the purpose of the census interview, answered questions, elicited information following a script, and recorded census data on forms.
+                    </li>
+                    <li>
+                    Assessed quality control levels on selected addresses, determined which samples passed or failed, and maintained records for quality control verification.
+                    </li>
+                    <li>
+                    Complied with accuracy standards while maintaining high production rates.
+                    </li>
+                    <li>
+                    Maintained records of hours worked, unit produced, miles driven, quality control results, and expenses incurred in the performance of duties.
+                    </li>
+                    <li>
+                    Met daily with the supervisor to review and submit work, and receive additional instructions.
                     </li>
                   </ul>
                 </p>
