fa49e215885e0ca333718613513c20037042c3c5
Felix Chan <felixchan29@gmail.com>
2020-11-02 12:37:46 -0500

    added meta tags

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.scss';
 import Portfolio from './containers/Portfolio';
 import { Responsive } from 'semantic-ui-react'
 
+import MetaTags from 'react-meta-tags';
+
+
 import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 
@@ -16,9 +19,17 @@ class App extends Component {
         <meta property='og:description' content="This is my portfolio"/>
         <meta property='og:url' content="https://felixchan.netlify.com/" /> */}
         <React.Fragment>
-            <Responsive {...Responsive.onlyMobile}>
-              <meta name="viewport" content="initial-scale=1, maximum-scale=1" />
-            </Responsive>      
+
+          <MetaTags>
+            <meta id="meta-description" name="description" content="Felix Chan personal website" />
+            <meta id="og-title" property="og:title" content="Personal website" />
+            <meta id="og-image" property="og:image" content="https://raw.githubusercontent.com/chanfe/portfolio_website/master/src/assets/Media_image.png" />
+          </MetaTags>
+
+          <Responsive {...Responsive.onlyMobile}>
+            <meta name="viewport" content="initial-scale=1, maximum-scale=1" />
+          </Responsive>  
+
           <Switch>
             <Route path="/" render={(props) => <Portfolio />} />
           </Switch>
